a6eaee3ff345eaf8f01f0fff54175f04cd9d8201
morshoto <jojoto8845@icloud.com>
2026-08-11 14:52:32 +0900

    update: agent browser installation

--- nix/packages.nix
+++ nix/packages.nix
@@ -3,6 +3,28 @@
 let
   hasTfenv = pkgs ? tfenv;
   terraformPackages = if hasTfenv then [ pkgs.tfenv ] else [ pkgs.terraform ];
+  pandocLua = pkgs.pandoc.overrideAttrs (oldAttrs: {
+    pname = "pandoc-with-lua";
+    configureFlags = (oldAttrs.configureFlags or [ ]) ++ [
+      "-f"
+      "lua"
+    ];
+  });
+  agentBrowser = pkgs.stdenv.mkDerivation {
+    pname = "agent-browser";
+    version = "0.33.2";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/vercel-labs/agent-browser/releases/download/v0.33.2/agent-browser-darwin-arm64";
+      hash = "sha256-y7UXkCvKo7emOE/Z8l3SdNo98rtqO6nD6FgG14ITwms=";
+    };
+
+    dontUnpack = true;
+
+    installPhase = ''
+      install -Dm755 "$src" "$out/bin/agent-browser"
+    '';
+  };
 
   packageList =
     (with pkgs; [
@@ -13,6 +35,7 @@ let
       curl
       nodejs_22
       pnpm
+      agentBrowser
       google-cloud-sql-proxy
       cocoapods
       diff-pdf
@@ -30,7 +53,7 @@ let
       kaggle
       llvmPackages.openmp
       maven
-      pandoc
+      pandocLua
       pdftk
       postgresql_16
       poppler-utils
